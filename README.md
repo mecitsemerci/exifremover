d2e7563579cf8cd2923530fa9e070ed719585ebc
Mecit Semerci <mecit.semerci@gmail.com>
2024-10-06 16:39:50 +0300

    add readme and add test image

---
+++ README.md
@@ -0,0 +1,55 @@
+# EXIF Metadata Remover
+
+A simple Go web application that removes EXIF metadata from uploaded images.
+
+## Installation
+
+To install the project, follow these steps:
+
+1. Install Go (version 1.16 or higher) if you haven't already. You can download it from the [official Go website](https://golang.org/dl/).
+
+2. Clone the project repository:
+
+```bash
+git clone https://github.com/mecitsemerci/exifremover.git
+```
+
+3. Navigate to the project directory:
+
+```bash
+cd exifremover
+```
+
+4. Build the project:
+
+```bash
+go build
+```
+
+## Usage
+
+To run the application, execute the following command:
+
+```bash
+./exifremover
+```
+
+The application will start listening on port 8080 by default. You can access it in your web browser by navigating to `http://localhost:8080`.
+
+To upload an image and remove its EXIF metadata, follow these steps:
+
+1. Click on the "Choose File" button.
+2. Select an image file from your computer.
+3. Click on the "Upload" button.
+4. After the upload is complete, you will see a download link below the image. Click on the link to download the image with the EXIF metadata removed.
+
+## Environment Variables
+
+You can customize the behavior of the application by setting environment variables. Here are some available options:
+
+- `PORT`: The port number on which the application will listen. Default is `8080`.
+- `LOGGING_ENABLED`: Set to "false" to disable logging. Default is "true".
+
+## Acknowledgments
+
+This project was inspired by the need to remove EXIF metadata from images for privacy and security reasons. The code uses the [goexif](https://github.com/rwcarlsen/goexif) library for reading and writing EXIF metadata, and the [imaging](https://github.com/disintegration/imaging) library for image processing.
--- main.go
+++ main.go
@@ -3,16 +3,55 @@ package main
 import (
 	"bytes"
 	"image/jpeg"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"log"
 
 	"github.com/disintegration/imaging"
 	"github.com/rwcarlsen/goexif/exif"
 )
 
+func main() {
+	// Serve the HTML page on the root path
+	http.HandleFunc("/", htmlHandler)
+
+	// Handle file upload and EXIF removal
+	http.HandleFunc("/upload", uploadHandler)
+
+	// Start the server on port
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Server listening on port %s", port)
+
+	err := http.ListenAndServe(":"+port, nil)
+
+	if err != nil {
+		log.Fatal("Server error:", err)
+	}
+}
+
+var logger *slog.Logger
+
+func init() {
+	// Check if the LOGGING_ENABLED environment variable is set to "false"
+	if os.Getenv("LOGGING_ENABLED") == "false" {
+		// Create a logger that discards all log messages
+		logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+	} else {
+		// Create a logger with default settings
+		logger = slog.Default()
+	}
+}
+
 // Upload handler to process the image and return it as a downloadable file
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
+		logger.Error("Invalid request method", "method", r.Method)
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
@@ -20,6 +59,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// Parse the multipart form
 	err := r.ParseMultipartForm(10 << 20) // Limit to 10MB
 	if err != nil {
+		logger.Error("Failed to parse form", "error", err)
 		http.Error(w, "Unable to parse form", http.StatusBadRequest)
 		return
 	}
@@ -27,6 +67,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// Get the uploaded file
 	file, _, err := r.FormFile("photo")
 	if err != nil {
+		logger.Error("Failed to get file from form", "error", err)
 		http.Error(w, "Unable to read file", http.StatusBadRequest)
 		return
 	}
@@ -34,18 +75,22 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Check if the image contains EXIF data
 	ex, err := exif.Decode(file)
-	
+
 	if err != nil {
+		logger.Error("Failed to decode EXIF data", "error", err)
 		http.Error(w, "EXIF data not found", http.StatusBadRequest)
-        return
+		return
 	}
+	logger.Info("EXIF data found", "exif", ex.String())
 
 	// Seek the file back to the beginning to read the image for further processing
 	file.Seek(0, 0)
 
 	// Decode the image
 	img, err := imaging.Decode(file)
+
 	if err != nil {
+		logger.Error("Failed to decode image", "error", err)
 		http.Error(w, "Failed to decode image", http.StatusInternalServerError)
 		return
 	}
@@ -54,6 +99,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	var imgBuffer bytes.Buffer
 	err = jpeg.Encode(&imgBuffer, img, nil)
 	if err != nil {
+		logger.Error("Failed to encode image", "error", err)
 		http.Error(w, "Failed to encode image", http.StatusInternalServerError)
 		return
 	}
@@ -65,23 +111,10 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	// Write the image to the response
 	w.Write(imgBuffer.Bytes())
 
-	// Optionally: Log whether EXIF data was found
-	log.Printf("EXIF found: %v\n", true)
-	log.Print(ex)
+	logger.Info("EXIF image removed successfully")
 
 }
 
 func htmlHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./index.html")
-}
-
-func main() {
-	// Serve the HTML page on the root path
-	http.HandleFunc("/", htmlHandler)
-
-	// Handle file upload and EXIF removal
-	http.HandleFunc("/upload", uploadHandler)
-
-	log.Println("Server starting on :8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
